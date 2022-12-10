ce5d0e75030f221d1879b9666a1e5f9d3b6bfa86
JeremyFerreira <91945288+JeremyFerreira@users.noreply.github.com>
2022-12-10 13:42:35 +0100

    push

--- Assets/Script/Input/SOInputButton.cs
+++ Assets/Script/Input/SOInputButton.cs
@@ -1,18 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
+[CreateAssetMenu(fileName = "InputData", menuName = "Button")]
 public class SOInputButton : MonoBehaviour
 {
     [SerializeField] bool value;
-    //public event Action OnPressed;
+    public event Action OnPressed;
+    public event Action OnReleased;
 
     public void OnButtonPressed(InputAction.CallbackContext ctx)
     {
         if (ctx.performed)
         {
             OnPressed?.Invoke();
+        }
+        value = ctx.performed;
+    }
+    public void OnButtonReleased(InputAction.CallbackContext ctx)
+    {
+        if (ctx.canceled)
+        {
+            OnReleased?.Invoke();
         }
         value = ctx.performed;
     }
