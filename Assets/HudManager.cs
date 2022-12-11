23ade5196bfbd80c29afde5c749e87ea8c665bb0
Fabrice C <47283393+fabrisek@users.noreply.github.com>
2022-12-11 17:04:28 +0100

    HUD

---
+++ Assets/HudManager.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Doozy.Runtime.UIManager.Containers;
+using Doozy.Runtime.UIManager.Components;
+public class HudManager : MonoBehaviour
+{
+    [Header("PANEL")]
+    [SerializeField] UIContainer _mainMenu;
+    [SerializeField] UIContainer _settings;
+    [SerializeField] UIContainer _levelSelectionPanel;
+    [SerializeField] UIContainer _worldSelectionPanel;
+    [SerializeField] UIContainer _pausePanel;
+    [SerializeField] UIContainer _InGamePanel;
+    [SerializeField] UIContainer _DeathPanel;
+    [SerializeField] UIContainer _winPanel;
+    [SerializeField] UIContainer _HighScorePanel;
+
+    [Header("EVENT")]
+    [SerializeField] EventSO _eventReachFinishLine;
+    [SerializeField] EventSO _eventStartLevel;
+    static bool created = false;
+    void Awake()
+    {
+        if (!created)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            created = true;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    private void OnEnable()
+    {
+        _eventReachFinishLine.OnLaunchEvent += OpenWinPanel;
+        _eventStartLevel.OnLaunchEvent += OpenInGamePanel;
+    }
+    private void OnDisable()
+    {
+        _eventReachFinishLine.OnLaunchEvent -= OpenWinPanel;
+        _eventStartLevel.OnLaunchEvent -= OpenInGamePanel;
+    }
+
+    private void OpenInGamePanel()
+    {
+        CloseAllPanel();
+        _InGamePanel.Show();
+    }
+
+    private void OpenWinPanel()
+    {
+        CloseAllPanel();
+        _winPanel.Show();
+    }
+
+    public void CloseAllPanel()
+    {
+        _mainMenu.Hide();
+        _settings.Hide();
+        _levelSelectionPanel.Hide();
+        _worldSelectionPanel.Hide();
+        _pausePanel.Hide();
+        _InGamePanel.Hide();
+        _DeathPanel.Hide();
+        _winPanel.Hide();
+        _HighScorePanel.Hide();
+    }
+}
