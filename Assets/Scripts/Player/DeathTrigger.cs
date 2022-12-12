fa6a943fc6d138366b8b1d3180e67ac4baf32e50
JeremyFerreira <91945288+JeremyFerreira@users.noreply.github.com>
2022-12-12 20:54:31 +0100

    push

--- Assets/Scripts/Player/DeathTrigger.cs
+++ Assets/Scripts/Player/DeathTrigger.cs
@@ -6,18 +6,27 @@ using UnityEngine;
 public class DeathTrigger : MonoBehaviour
 {
     [SerializeField] private EventSO _deathEvent;
+    [SerializeField] private EventSO _winEvent;
     [SerializeField] private EventSO _StartRunEvent;
     [SerializeField] private EventSO _eventStopInput;
     [SerializeField] private LayerMask _layersDeath;
 
     bool _IsAlive;
+    bool _IsWin;
     private void OnEnable()
     {
         _StartRunEvent.OnLaunchEvent += Initialized;
+        _winEvent.OnLaunchEvent += Win;
+        _IsWin = false;
     }
     private void OnDisable()
     {
         _StartRunEvent.OnLaunchEvent -= Initialized;
+        _winEvent.OnLaunchEvent -= Win;
+    }
+    private void Win()
+    {
+        _IsWin = true;
     }
     private void Initialized()
     {
@@ -27,7 +36,7 @@ public class DeathTrigger : MonoBehaviour
     {
         if ((_layersDeath & (1 << other.gameObject.layer)) != 0)
         {
-            if (_IsAlive)
+            if (_IsAlive && !_IsWin)
             {
                 _IsAlive = false;
                 _deathEvent.OnLaunchEvent?.Invoke();
