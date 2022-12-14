41b69037ef2fef9d38f6378484ea40dca05c49d2
JeremyFerreira <91945288+JeremyFerreira@users.noreply.github.com>
2022-12-15 00:05:33 +0100

    push

--- Assets/WinPanelHUD.cs
+++ Assets/WinPanelHUD.cs
@@ -3,29 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class WinPanelHUD : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI _textScore;
     [SerializeField] TextMeshProUGUI _textBestScore;
+    [SerializeField] TextMeshProUGUI _timeBetweenScore;
+    [SerializeField] TextMeshProUGUI _textNewHighScore;
     [SerializeField] TextMeshProUGUI _textPosPlayer;
     [SerializeField] EventSO _eventReachLine;
     [SerializeField] EventSO _eventChangePos;
     [SerializeField] TimeSO _timer;
+    [SerializeField] Image currentMedal;
+    [SerializeField] Sprite[] spritesMedal;
+    Animator animator;
+    int medal;
+    public void SetMedal(int i)
+    {
+        medal = i;
+    }
+    [SerializeField] EventSO _eventHighScore;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _textNewHighScore.enabled = false;
+        animator = GetComponent<Animator>();
     }
     private void OnEnable()
     {
         _eventReachLine.OnLaunchEvent += ReachLine;
         _eventChangePos.OnLauchEventInt += ChangePosPlayer;
+        _eventHighScore.OnLaunchEvent += UpdateUIHighScore;
     }
     private void OnDisable()
     {
         _eventReachLine.OnLaunchEvent -= ReachLine;
-        _eventChangePos.OnLauchEventInt += ChangePosPlayer;
+        _eventChangePos.OnLauchEventInt -= ChangePosPlayer;
+        _eventHighScore.OnLaunchEvent -= UpdateUIHighScore;
     }
     void ChangePosPlayer(int pos)
     {
@@ -36,5 +51,22 @@ public class WinPanelHUD : MonoBehaviour
         SceneSO data = DataManager.Instance.GetSceneData(SceneManager.GetActiveScene().buildIndex);
         _textScore.text = TimerFormat.FormatTime(_timer.TotalTime);
         _textBestScore.text = "BEST TIME : " + TimerFormat.FormatTime(data.BestTime);
+
+        float difference = data.BestTime - _timer.TotalTime;
+        _timeBetweenScore.text = difference < 0 ? "+ " + TimerFormat.FormatTime(Mathf.Abs(difference)) : "- " + TimerFormat.FormatTime(Mathf.Abs(difference));
+        _timeBetweenScore.color = difference < 0 ? Color.red : Color.blue;
+        animator.CrossFade("WinPanel", 0, 0);
+    }
+    private void UpdateUIHighScore()
+    {
+        if (medal <= 2)
+        {
+            currentMedal.sprite = spritesMedal[medal];
+        }
+        else
+        {
+            currentMedal.sprite = null;
+        }
+        _textNewHighScore.enabled = true;
     }
 }
